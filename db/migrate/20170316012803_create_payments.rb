19344f59a11da06fa4b9b3dd311d2bea40942237
Raghu Betina <raghu@firstdraft.com>
2017-03-16 01:28:03 +0000

    Generate payments resource with starter:resource

---
+++ db/migrate/20170316012803_create_payments.rb
@@ -0,0 +1,13 @@
+class CreatePayments < ActiveRecord::Migration
+  def change
+    create_table :payments do |t|
+      t.integer :user_id
+      t.integer :amount
+      t.integer :other_id
+      t.string :description
+
+      t.timestamps
+
+    end
+  end
+end
